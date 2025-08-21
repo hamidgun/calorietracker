eed9d4d7e262a84c580af2e15754cbcb9ec5e3c5
Hamid Gunaydin <hamid.gunaydin@powercoders.org>
2025-08-21 21:45:39 +0200

    Header has been formed by Hamid G.

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      id="header"
+      className="d-flex justify-content-between align-items-center shadow rounded"
+    >
+      <div className="container mt-3">
+        <div className="row text-center align-items-center justify-content-center g-3 text-white">
+          <div className="col-4">
+            <div>
+              <h6 className="value">1540</h6>
+              <p className="label">INTAKE</p>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className=" d-flex flex-column align-items-center">
+              <div className="circle-ring mb-2 d-flex flex-column align-items-center justify-content-center">
+                <p className="value">200</p>
+                <small className="label">KCAL OVER</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div>
+              <h6 className="value">420</h6>
+              <p className="label">BURNED</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
