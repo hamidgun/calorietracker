79d6b45186430e5adc8cd3385de61bd0842e8678
Hamid Gunaydin <hamid.gunaydin@powercoders.org>
2025-08-21 23:22:17 +0200

    Date section has been added by Hamid G.

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,23 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="date rounded mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <i className="bi bi-arrow-left fs-4"></i>
+            </div>
+            <div className="col-4">
+              <h5 className="text-center">
+                <i className="bi bi-calendar"></i> Today, 21 Aug
+              </h5>
+            </div>
+            <div className="col-4 text-end">
+              <i className="bi bi-arrow-right fs-4 "></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
