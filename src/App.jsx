c9cd3583eb3c801e8b50b95deb77352411595aa7
Hamid Gunaydin <hamid.gunaydin@powercoders.org>
2025-08-22 22:12:39 +0200

    footer has been added by Hamid Gunaydin.

--- src/App.jsx
+++ src/App.jsx
@@ -179,6 +179,36 @@ function App() {
           </div>
         </div>
       </div>
+      <footer>
+        <nav className="bottom-nav">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-3 p-3">
+              <a href="#" className="nav-link text-white text-center">
+                <i className="bi bi-journal-text"></i>
+                <div className="nav-label">Diary</div>
+              </a>
+            </div>
+            <div className="col-3 p-3">
+              <a href="#" className="nav-link text-white text-center">
+                <i className="bi bi-star"></i>
+                <div className="nav-label">Favorites</div>
+              </a>
+            </div>
+            <div className="col-3 p-3">
+              <a href="#" className="nav-link text-white text-center">
+                <i className="bi bi-plus-circle-fill"></i>
+                <div className="nav-label">Create</div>
+              </a>
+            </div>
+            <div className="col-3 p-3">
+              <a href="#" className="nav-link text-white text-center">
+                <i className="bi bi-person"></i>
+                <div className="nav-label">Profile</div>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </footer>
     </>
   );
 }
